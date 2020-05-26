317ad38dc1229f1343ced644c8df6d9c1f802901
Chris-G-Bowles <chris.g.bowles@gmail.com>
2020-05-25 21:57:50 -0700

    Completely reworked the program error-checking flow.
    The program now works once again.

--- FileComparator.java
+++ FileComparator.java
@@ -20,6 +20,12 @@ public class FileComparator {
 		} else {
 			file1Location = args[0];
 		}
+		FileInputStream inputStream1 = null;
+		try {
+			inputStream1 = new FileInputStream(file1Location);
+		} catch (Exception e) {
+			error(file1Location + " is not a valid file.");
+		}
 		String file2Location;
 		if (args.length == 0) {
 			System.out.print("Enter file 2's location: ");
@@ -27,54 +33,59 @@ public class FileComparator {
 		} else {
 			file2Location = args[1];
 		}
-		input.close();
+		FileInputStream inputStream2 = null;
 		try {
-			FileInputStream inputStream = new FileInputStream(file1Location);
-			int token;
-			ArrayList<Integer> file1 = new ArrayList<>();
-			do {
-				try {
-					token = inputStream.read();
-				} catch (Exception e) {
-					token = -1;
-				}
-				if (token >= 0 && token <= 255) {
-					file1.add(token);
-				}
-			} while (token >= 0 && token <= 255);
-			inputStream.close();
-			try {
-				inputStream = new FileInputStream(file2Location);
-				ArrayList<Integer> file2 = new ArrayList<>();
-				do {
-					try {
-						token = inputStream.read();
-					} catch (Exception e) {
-						token = -1;
-					}
-					if (token >= 0 && token <= 255) {
-						file2.add(token);
-					}
-				} while (token >= 0 && token <= 255);
-				inputStream.close();
-				System.out.println("\n" + file1Location + ": " + file1.size() + " bytes");
-				System.out.println(file2Location + ": " + file2.size() + " bytes");
-				System.out.println("\nBinary differences:");
-				System.out.println("Address   File 1   File 2");
-				int i;
-				for (i = 0; i < file1.size() && i < file2.size(); i++) {
-					if (!file1.get(i).equals(file2.get(i))) {
-						System.out.printf("%7s   %6s   %6s\n", hexadecimal(i), hexadecimal(file1.get(i)),
-								hexadecimal(file2.get(i)));
-					}
-				}
-				System.out.println("\n" + i + " bytes checked.");
-			} catch (Exception e) {
-				System.out.println("Error: " + file2Location + " not found.");
-			}
+			inputStream2 = new FileInputStream(file2Location);
 		} catch (Exception e) {
-			System.out.println("Error: " + file1Location + " not found.");
+			error(file2Location + " is not a valid file.");
 		}
+		input.close();
+		int token1;
+		ArrayList<Integer> file1 = new ArrayList<>();
+		do {
+			try {
+				token1 = inputStream1.read();
+			} catch (Exception e) {
+				token1 = -1;
+			}
+			if (token1 >= 0 && token1 <= 255) {
+				file1.add(token1);
+			}
+		} while (token1 >= 0 && token1 <= 255);
+		try {
+			inputStream1.close();
+		} catch (Exception e) {
+			error("Unable to close inputStream1.");
+		}
+		int token2;
+		ArrayList<Integer> file2 = new ArrayList<>();
+		do {
+			try {
+				token2 = inputStream2.read();
+			} catch (Exception e) {
+				token2 = -1;
+			}
+			if (token2 >= 0 && token2 <= 255) {
+				file2.add(token2);
+			}
+		} while (token2 >= 0 && token2 <= 255);
+		try {
+			inputStream2.close();
+		} catch (Exception e) {
+			error("Unable to close inputStream2.");
+		}
+		System.out.println("\n" + file1Location + ": " + file1.size() + " bytes");
+		System.out.println(file2Location + ": " + file2.size() + " bytes");
+		System.out.println("\nBinary differences:");
+		System.out.println("Address   File 1   File 2");
+		int i;
+		for (i = 0; i < file1.size() && i < file2.size(); i++) {
+			if (!file1.get(i).equals(file2.get(i))) {
+				System.out.printf("%7s   %6s   %6s\n", hexadecimal(i), hexadecimal(file1.get(i)),
+						hexadecimal(file2.get(i)));
+			}
+		}
+		System.out.println("\n" + i + " bytes checked.");
 	}
 	
 	private static String hexadecimal(int number) {
